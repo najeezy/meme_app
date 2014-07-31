e20fa00b459ec251df7882ab16d1a1832cffface
truotaku <najee.s.gardner@gmail.com>
2014-07-31 16:36:02 -0400

    worked on new and create routes and controller actions

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,5 +1,15 @@
 class MemesController < ApplicationController
 
-  
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = Meme.create(memes_params)
+  end
+
+  def memes_params
+    params.require(:meme).permit(:name, :humor_level, :image_url)
+  end
 
 end
